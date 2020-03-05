2d3253337c15ab268e7cdf6da22bd34129be7b9e
idkondor <odis.kondor@gmail.com>
2020-03-05 10:16:28 +0200

    added directories walker

--- mp3_silence_extractor.py
+++ mp3_silence_extractor.py
@@ -3,20 +3,29 @@ import os
 
 parser = ArgumentParser()
 parser.add_argument("-in", "--folderIn", dest="pathToMp3", help="Input folder path with files to extract silence!")
-parser.add_argument("-out", "--folderOut", dest="convertedMp3Path", help="Input folder path where to put converted "
-                                                                         "files!")
 parser.add_argument("-p", "--pauseTime", dest="pauseTime", help="How many seconds has to be pause span between sounds!")
 args = parser.parse_args()
 pathToMp3 = args.pathToMp3
-convertedMp3Path = args.convertedMp3Path
 pauseTime = args.pauseTime
+dirOrFilesLst = list()
 
+for (dirPath, dirNames, audioNames) in os.walk(pathToMp3):
+    dirOrFilesLst += [os.path.join(dirPath, file) for file in audioNames]
 
-for mp3Track in os.listdir(pathToMp3):
-    print(f"Converting {mp3Track} ......")
-    mp3TrackPath = os.path.join(pathToMp3, mp3Track)
-    if "m4a" in mp3Track:
-        os.system(f"ffmpeg -i '{mp3TrackPath}' -f sox - | sox -p "
-                  f"'{convertedMp3Path}/s_{mp3Track.replace('m4a', 'mp3')}' silence 1 0.1 1% -1 {pauseTime} 1%")
-    else:
-        os.system(f"sox '{mp3TrackPath}' '{convertedMp3Path}/s_{mp3Track}' silence 1 0.1 1% -1 {pauseTime} 1%")
+for path in dirOrFilesLst:
+    audioFileName = path.split("/")[-1]
+    silenceSlicedDir = path.replace(audioFileName, "") + "Silence_sliced"
+    if ".mp3" in path:
+        if not os.path.isdir(silenceSlicedDir):
+            os.mkdir(silenceSlicedDir)
+
+        os.system(f"sox '{path}' '{silenceSlicedDir}/s_{audioFileName}' silence 1 0.1 1% -1 "
+                  f"{pauseTime} 1%")
+
+    elif ".m4a" in path:
+        if not os.path.isdir(silenceSlicedDir):
+            os.mkdir(silenceSlicedDir)
+        print(f"Converting {path} ......")
+        os.system(f"ffmpeg -i '{path}' -f sox - | sox -p "
+                  f"'{silenceSlicedDir}/s_{audioFileName.replace('m4a', 'mp3')}' silence 1 0.1 1% -1 "
+                  f"{pauseTime} 1%")
